7ac2d5bcfde5a961e20efeced382be1020304741
jordynelson-2 <nelsonjordy2@gmail.com>
2023-02-25 11:46:05 +1100

    Changed projectID in sanity.client.ts to be explicit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const projectId = "vi9mlibn"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineCliConfig({
