c6537c43093d142f3b9c6ad3d61cc5730b224dca
shepherd-moves <admin@shepherdmoves.com.au>
2023-03-09 12:21:52 +1100

    changed text on the home page to test if sm github works

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const features = [
     icon: AiFillHome,
   },
   {
-    name: "House Staging and Styling",
+    name: "House Staging",
     description:
       "We have worked with multiple staging companies since our inception. We pride ourselves on the extra care we take to ensure the staged properties are utterly perfect.",
     icon: GiBedLamp,
