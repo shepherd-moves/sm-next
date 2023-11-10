88d5f62cab96fe38fca3193a7a4b0f55d937db4f
shepherd-moves <127360393+shepherd-moves@users.noreply.github.com>
2023-11-10 14:23:02 +1100

    Added aria-labelled by tags to buttons with no context
    Added aria-labelled by tags to buttons with no context

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,36 +12,42 @@ import { motion } from "framer-motion";
 const features = [
   {
     name: "Residential Homes and Apartments",
+    aria_text: "Read more about Residential Homes and Apartments",
     description:
       "Shepherd Moves started out with residential moves. Our team has built the skills and equipment to ensure your valuable possessions are delivered carefully and in a timely manner. At shepherd moves we always go above and beyond and promise to go the extra mile to ensure our customers are satisfied. We pride ourselves on making moving as stress free and easy as possible! Weather you’re moving from an apartment or a home we have you covered. With many different services available we have you covered",
     icon: AiFillHome,
   },
   {
     name: "House Staging and Styling",
+    aria_text: "Read more about House Staging and Styling",
     description:
       "We have worked with multiple staging companies since our inception. We pride ourselves on the extra care we take to ensure the staged properties are utterly perfect.",
     icon: GiBedLamp,
   },
   {
     name: "Office Relocations",
+    aria_text: "Read more about Office Relocations",
     description:
       "Our team has performed many office relocations in Melbourne and surrounding Victoria. We handle everything from dismantling, assembly, permits and all other logistical issues with both building sites. We are here to help you relocate offices with ease and have you moved and ready to operate in your new workspace as quickly and safely as possible. For more information please don’t hesitate to contact one of our amazing staff to help you with your next office move.",
     icon: HiOfficeBuilding,
   },
   {
     name: "Warehouse Relocations",
+    aria_text: "Read more about Warehouse Relocations",
     description:
       "Shepherd Moves has the equipment, skills and vehicles necessary to move any type of warehouse in full. Our team specialises in moving heavy machinery, car hoists, racking and all types of tools and furniture to make sure your inventory is relocated safely.",
     icon: FaWarehouse,
   },
   {
     name: "Interstate Relocations",
+    aria_text: "Read more about Interstate Relocations",
     description:
       "Shepherd Moves has the equipment, expertise and vehicles necessary to move any type of warehouse in full. Our team specialises in moving heavy machinery, car hoists, racking and all types of tools and furniture to make sure your inventory is relocated safely. With our knowledge of warehouse moves, we can help you relocate with very little disruption to your business. For more information please don’t hesitate to contact one of our amazing staff to help you with your next office move.",
     icon: FaMapMarkedAlt,
   },
   {
     name: "Racking Solutions",
+    aria_text: "Read more about Racking Solutions",
     description:
       "Our team can install all types of racking for any storage purposes. With our tenure in the logistics and storage space, we also offeradvise on what the best options for your storage needs are, and can provide and install racking to your specifications. For more information please don’t hesitate to contact one of our amazing staff to help you with your next office move.",
     icon: BsBookshelf,
@@ -103,6 +109,7 @@ export default function Home({ reviews }: any) {
                   whileTap={{ scale: 0.9 }}
                 >
                   <Link
+                    aria-labelledby={feature.aria_text}
                     href={`our-services/${feature.name
                       .replace(/\s/g, "-")
                       .toLowerCase()}`}
