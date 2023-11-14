abefeedbf04a8f2be42c8943160591f01adad7ba
shepherd-moves <127360393+shepherd-moves@users.noreply.github.com>
2023-11-14 13:55:07 +1100

    Changed colour of Google button
    To create contrast for accessability purposes

--- pages/index.tsx
+++ pages/index.tsx
@@ -159,7 +159,7 @@ export default function Home({ reviews }: any) {
                   type="button"
                   href="https://maps.app.goo.gl/cAzTVtFfZUXBt2MP8"
                   target="_blank"
-                  className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
+                  className="text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
                 >
                   <svg
                     className="w-4 h-4 me-2 mr-2"
